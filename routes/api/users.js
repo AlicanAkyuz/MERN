75dc40b1e541c29b3276f01c25c15d4e47a34198
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-25 19:42:57 -0700

    User model schema.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// Load user model
+const User = require("../../models/User");
 
 // @route:  GET for api/users/test
 // @desc:   tests users route
 // @access: public
 router.get("/test", (req, res) => res.json({ msg: "users work for now" }));
+
+// @route:  GET for api/users/register
+// @desc:   register a user
+// @access: public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
