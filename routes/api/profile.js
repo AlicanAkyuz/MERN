5426260feb13a2d42365bdd620cbab66fbf0d262
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-27 11:31:47 -0700

    profile validation.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load profile model of mongoose
 const Profile = require("../../models/Profile");
 // Load user model of mongoose
@@ -22,6 +25,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile found for this user";
@@ -35,13 +39,37 @@ router.get(
   }
 );
 
-// @route:  GET for api/profile
+// @route:  GET for api/profile/handle/:handle
+// @desc:   get profile by handle
+// @access: public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route:  POST api/profile
 // @desc:   create and update user's profile
 // @access: private
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
