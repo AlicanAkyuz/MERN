458f81b19a9e8d281c7a9c691f970a8b446d6ba3
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-27 09:08:30 -0700

    update & create profile

---
+++ routes/api/profile.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile model of mongoose
+const Profile = require("../../models/Profile");
+// Load user model of mongoose
+const User = require("../../models/User");
+
+// @route:  GET for api/profile/test
+// @desc:   tests profiles route
+// @access: public
+router.get("/test", (req, res) => res.json({ msg: "profiles work for now" }));
+
+// @route:  GET for api/profile
+// @desc:   get current user's profile
+// @access: private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile found for this user";
+          res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  }
+);
+
+// @route:  GET for api/profile
+// @desc:   create and update user's profile
+// @access: private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername) {
+      profileFields.githubusername = req.body.githubusername;
+    }
+
+    // Skills - split into array
+    if (typeof req.body.skills !== undefined) {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // Social fields
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Profile already exists, update profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Profile does not exist, create profile
+
+        // check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // save profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
+
+module.exports = router;
--- routes/api/profiles.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route:  GET for api/profiles/test
-// @desc:   tests profiles route
-// @access: public
-router.get("/test", (req, res) => res.json({ msg: "profiles work for now" }));
-
-module.exports = router;
