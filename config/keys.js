88fdd3106e1593613548769fb94fc1f9e9af9d3b
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-26 22:02:53 -0700

    route to register user

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://alican123:alican123@alicanmern-xpujy.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://alican123:alican123@alicanmern-xpujy.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
-// Load user model
+// Load user model of mongoose
 const User = require("../../models/User");
 
 // @route:  GET for api/users/test
@@ -16,6 +18,7 @@ router.get("/test", (req, res) => res.json({ msg: "users work for now" }));
 // @access: public
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
+    // check if email already exists
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
@@ -43,6 +46,39 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route:  GET for api/users/login
+// @desc:   finds user by email in db, checks for pwd and returns Json token
+// @access: public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found!" });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // user matched: create jwt payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+
+        //sign token through jsonwebtoken
+        jwt.sign(payload, keys.secretOrKey);
+      } else {
+        return res.status(400).json({ password: "password incorrect" });
+      }
+    });
   });
 });
 
