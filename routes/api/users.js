78c11b1b85ee222faf70861beef66b3f6e8dc5ed
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-26 22:29:53 -0700

    private route for users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load user model of mongoose
 const User = require("../../models/User");
@@ -74,7 +75,17 @@ router.post("/login", (req, res) => {
         };
 
         //sign token through jsonwebtoken
-        jwt.sign(payload, keys.secretOrKey);
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "password incorrect" });
       }
@@ -82,4 +93,18 @@ router.post("/login", (req, res) => {
   });
 });
 
+// @route:  GET for api/users/current
+// @desc:   return current user
+// @access: private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profiles");
@@ -21,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDb Conencted"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hi there"));
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
