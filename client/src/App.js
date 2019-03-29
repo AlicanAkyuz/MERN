b7e5ef40cf679d991c724a9c4217680ed4585982
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-28 21:12:13 -0700

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <h1>react app</h1>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
