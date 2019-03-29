fd4bbf48db2441de02bea943a499d07d3cd30c72
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-28 20:57:09 -0700

    nevbar and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>react app</h1>
+        <Footer />
       </div>
     );
   }
