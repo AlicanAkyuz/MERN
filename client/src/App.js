483ddf88835017dc2a0a4966f2e55234f9613c95
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-03-28 21:45:55 -0700

    login and register state management

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import "./App.css";
 
@@ -13,7 +15,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Route path="/" component={Landing} />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
