d94c512e2f7ea8f1a50dbdbf42823c393ddce2dc
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-01 12:21:50 -0700

    profile reducer set up

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,2 @@
+import axios from "axios";
+import { GET_ERRORS, GET_PROFILE, PROFILE_LOADING } from "./types";
---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  profile: null,
+  profiles: null,
+  loadimg: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
