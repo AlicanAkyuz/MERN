46103fbe5a9a55888dde10bfd4ffe84f56d51857
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-01 14:33:58 -0700

    create user profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -3,9 +3,40 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInput: false,
+      handle: "",
+      compnay: "",
+      website: "",
+      location: "",
+      status: "",
+      skills: "",
+      githubusername: "",
+      bio: "",
+      twitter: "",
+      facebook: "",
+      linkedin: "",
+      youtube: "",
+      instagram: "",
+      errors: {}
+    };
+  }
+
   render() {
     return <div />;
   }
 }
 
-export default connect(null)(CreateProfile);
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateTpProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateTpProps)(CreateProfile);
