9e07b10a918436b49e98a778c83f9292fa92d805
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-02 14:35:17 -0700

    display experience on dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from "../common/Spinner";
 import ProfileActions from "./ProfileActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -34,6 +36,7 @@ class Dashboard extends Component {
               <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
             </p>
             <ProfileActions />
+            <Experience experience={profile.experience} />
             {/* experience and education */}
             <div style={{ marginBottom: "60px" }}>
               <button
---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+class Education extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Education;
