e0e68d874a3e9c56d76b6c9eb185a97843d787db
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-03 11:49:54 -0700

    developer profiles layout

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import Spinner from "../common/Spinner";
+import { getProfiles } from "../../actions/profileActions";
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItem;
+
+    if (profiles === null || loading) {
+      profileItem = <Spinner />;
+    } else {
+      if (profiles.length > 0) {
+        profileItem = <h1>Profiles here:</h1>;
+      } else {
+        profileItem = <h4>No profiles found...</h4>;
+      }
+    }
+
+    return (
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">
+                Developer Profiles
+                <p className="lead text-center">
+                  Browse and Connect with Developers
+                </p>
+                {profileItem}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfiles }
+)(Profiles);
