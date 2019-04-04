a99d552d910482d81873974a949da879b0c4e70c
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-03 18:53:11 -0700

    profile credentials top

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,13 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Moment from 'react-moment';
 
 class ProfileCreds extends Component {
   render() {
-    return (
-      <div>
-        <h2>Profile Creds</h2>
-      </div>
-    );
-  }
-}
+    const { experience, education } = this.props;
 
-export default ProfileCreds;
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? (
+            ' Now'
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Position:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === '' ? null : (
+            <span>
+              <strong>Location: </strong> {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === '' ? null : (
+            <span>
+              <strong>Description: </strong> {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
