d39697bb053df91f005dd5a1a22bee02e79e8d2c
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-03 18:20:22 -0700

    profile actions.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,42 +3,42 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { getProfileByHandle } from "../../actions/profileActions"
+import { getProfileByHandle } from "../../actions/profileActions";
 import ProfileAbout from "./ProfileAbout";
 import ProfileCreds from "./ProfileCreds";
 import ProfileGithub from "./ProfileGithub";
 import ProfileHeader from "./ProfileHeader";
 import Spinner from "../common/Spinner";
 
-export default class Profile extends Component { 
+class Profile extends Component {
   componentDidMount() {
-      if (this.props.match.params.handle) {
-          this.props.getProfileByHandle(this.props.match.params.handle)
-      }
-
-      
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
   }
 
   render() {
-
     return (
-        <div>
-            Hello
-        </div>
-    )
+      <div>
+        <ProfileHeader />
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileHeader />
+      </div>
+    );
   }
 }
 
 Profile.propTypes = {
-    getProfileByHandle: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired
-  };
-  
-  const mapStateToProps = state => ({
-    profile: state.profile
-  });
-  
-  export default connect(
-    mapStateToProps,
-    { getProfileByHandle }
-  )(Profile);
+  getProfileByHandle: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
