5289baa184bdc1b7dc36b09691fea825003ab4e0
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-04-10 19:53:40 -0700

    preparation for deploy.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://alican123:alican123@alicanmern-xpujy.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profile");
@@ -32,6 +33,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profiles);
 app.use("/api/posts", posts);
+
+// Set server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
